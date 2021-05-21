53bb8ded7f03f1f6587d7cabf46e2a1a3771f7c6
DuplicateRabbit <smkjallad2@gmail.com>
2021-05-21 14:03:07 +0400

    Game upload
    YES INDEED

---
+++ Assets/BallsFromHeaven.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BallsFromHeaven : MonoBehaviour
+
+{
+
+    public GameObject ball;
+
+    public float startHeight = 28f;
+
+    public float fireInterval = 0.0f;
+
+    private float nextBallTime = 0.0f;
+
+
+
+    // Start is called before the first frame update
+
+    void Start()
+
+    {
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        if (Time.time > nextBallTime)
+
+        {
+
+            nextBallTime = Time.time + fireInterval;
+
+            Vector3 position = new Vector3(Random.Range(-4.0f, 4.0f), startHeight, Random.Range(-4.0f, 4.0f));
+
+            Instantiate(ball, position, Quaternion.identity);
+
+
+
+        }
+    }
+}
